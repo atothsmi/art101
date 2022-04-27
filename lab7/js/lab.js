81e3d9d72f4379abf56556a77900e452f0df7646
atothsmi <102501875+atothsmi@users.noreply.github.com>
2022-04-26 17:04:19 -0700

    Lab 7 Changes

---
+++ lab7/js/lab.js
@@ -0,0 +1,28 @@
+
+/*
+ * Practice creating Functions
+ * Author: Alia Toth-Smith
+ * Date: 4/26/2022
+
+
+/*
+ * Sorts letters of a user's input
+ * Parameters: userName - string
+ * Returns:
+*/
+function nameSort(userName){
+  //Split userName into an array of characters
+  var splitUserName = userName.split('');
+  //Poorly sort userName alphabetically and by case
+  splitUserName.sort();
+  //Rejoin userName to string and return it
+  return splitUserName.join('');
+}
+
+
+//Prompt user for their name
+var userName = window.prompt("Enter a username!");
+
+//Display user's name on the page
+document.writeln( "<div id='lab7-output'>Your Sorted Username:<br>",
+    nameSort(userName), "<br></div>")
