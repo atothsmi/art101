62eb8091020295309b4ef8c79e98f4656b48881b
atothsmi <102501875+atothsmi@users.noreply.github.com>
2022-04-28 09:28:18 -0700

    Update lab7 JS: Sorts by Case then Alphabetically

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -13,8 +13,22 @@
 function nameSort(userName){
   //Split userName into an array of characters
   var splitUserName = userName.split('');
-  //Poorly sort userName alphabetically and by case
-  splitUserName.sort();
+  //Poorly sort userName by case
+  splitUserName.sort()
+
+  //Sort by alphabetically only, ignoring case
+  splitUserName.sort(function(a,b){
+    var adjA = a.charCodeAt(0);
+    var adjB = b.charCodeAt(0);
+
+    if (a.charCodeAt(0) > 90){
+        adjA = a.charCodeAt(0) - 32;
+    }
+    if (b.charCodeAt(0) > 90){
+        adjB = b.charCodeAt(0) - 32;
+    }
+    return adjA - adjB;
+});
   //Rejoin userName to string and return it
   return splitUserName.join('');
 }
