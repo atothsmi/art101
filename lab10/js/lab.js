fa2402db8df9ddf550540db47635a22560a0cc31
atothsmi <102501875+atothsmi@users.noreply.github.com>
2022-05-05 16:43:49 -0700

    Comments for Lab 10 js

---
+++ lab10/js/lab.js
@@ -0,0 +1,13 @@
+/* Experiments with JS Events
+ * Sorts a user's name after a button press
+ * Author: Alia Toth-Smith
+ * Date: 5/6/2022
+ */
+
+// Borrow the nameSort() function or my anagram() function from Lab 7.
+
+
+// Attach an event listener to your button:
+  // Gets the value of your input field
+  // Runs that value through your sort() or anagram() function and saves the results.
+  // Replaces the html in <div id=output> with the results.
