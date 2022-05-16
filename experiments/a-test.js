a010008b7147f37314cc9e447a562094c95928cb
atothsmi <102501875+atothsmi@users.noreply.github.com>
2022-05-16 10:24:25 -0700

    Testing

---
+++ experiments/a-test.js
@@ -0,0 +1,1 @@
+/* Tests for features on our final project website */
---
+++ lab13/js/lab.js
@@ -0,0 +1,48 @@
+/*
+ * FizzBuzz function that finds multiples
+ * Author: Alia Toth-Smith
+ * Date: 5/17/2022
+ */
+
+var maxNum = 200;
+
+var multiples = [
+  {num: 3, message: 'Fizz'},
+  {num: 5, message: 'Buzz'},
+  {num: 7, message: 'Boom'}
+];
+
+//Loops through every number from 1 to maxNum
+ for (var i = 1; i <= maxNum; i++){
+   //console.log(i);
+
+   //Create an empty string to hold the message
+   var exclaimation = '';
+
+   //Loops through every item of the array multiples
+   for (var j = 0; j < multiples.length; j++){
+
+     //Retrieves an object of multiples to variable mult
+     var multiple = multiples[j];
+     //console.log(multiple);
+
+     //If current number is a multiple of the specified number,
+     //add that number's message to the final exclaimation
+     if (i % multiple.num == 0){
+       exclaimation += multiple.message;
+     };
+
+   };
+
+   //Adds exclaimation mark if number is a multiple
+   //of any of the inputed multiples
+   if (exclaimation !== ''){
+     exclaimation += '!';
+   };
+
+
+   $('#output').append('<p>' + i + ': ' + exclaimation);
+   console.log(i + ': ' + exclaimation);
+
+
+};
